f73f8a6d0f5d47f9a5587f389e51dbf0ee9cd912
anxpara <anxpara@gmail.com>
2024-03-06 23:48:45 -0800

    [TG] switch to vercel adapter and update npm packages

--- trialgrounds/svelte.config.js
+++ trialgrounds/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
   },
 };
 
