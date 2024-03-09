7d6b8b6d3b06aa38f4cf8fb849313eec9af46df2
anxpara <anxpara@gmail.com>
2024-03-09 09:05:26 -0800

    [TG] install meltUi

--- trialgrounds/svelte.config.js
+++ trialgrounds/svelte.config.js
@@ -1,9 +1,10 @@
 import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess({})],
+  preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
 
   kit: {
     adapter: vercel(),
