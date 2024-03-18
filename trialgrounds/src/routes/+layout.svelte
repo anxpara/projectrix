21d286385deb734dec56a70fd4c7cdf444fc2f31
anxpara <anxpara@gmail.com>
2024-03-18 15:17:07 -0700

    [TG] move menus into a melt collapsible

--- trialgrounds/src/routes/+layout.svelte
+++ trialgrounds/src/routes/+layout.svelte
@@ -8,6 +8,7 @@
   import OptionCheckbox from '../components/melt/OptionCheckbox.svelte';
   import { goto } from '$app/navigation';
   import { browser } from '$app/environment';
+  import { createCollapsible, melt } from '@melt-ui/svelte';
 
   export let data;
 
@@ -15,6 +16,11 @@
   setContext('options', options);
 
   $: viewingTrials = !$page.url.pathname.includes('demos');
+
+  const {
+    elements: { root, content, trigger },
+    states: { open },
+  } = createCollapsible();
 
   if (browser) {
     window.addEventListener('popstate', handlePopstate);
@@ -42,36 +48,50 @@
 </script>
 
 {#if !$options.hideUI}
-  <div class="centerer">
-    <h1 class="title">Projectrix Trialgrounds</h1>
-    <div class="menus-container">
-      <nav aria-labelledby="navTitle">
-        <p id="navTitle">directory</p>
-        <ul>
-          <li>
-            <a href="/{$page.url.search}">all trials</a>
-          </li>
-          <li>
-            <a href="/demos{$page.url.search}">all demos</a>
-          </li>
-        </ul>
-      </nav>
-      <div role="menu" aria-labelledby="menuTitle">
-        <p id="menuTitle">options</p>
-        <div class="option-groups">
-          <fieldset aria-label="common options">
-            {#each sharedOptionNames as name}
-              <OptionCheckbox {name}></OptionCheckbox>
-            {/each}
-          </fieldset>
-          <fieldset aria-label="trial options">
-            {#each trialOptionNames as name}
-              <OptionCheckbox {name} irrelevant={!viewingTrials}></OptionCheckbox>
-            {/each}
-          </fieldset>
+  <div use:melt={$root} class="centerer">
+    <div use:melt={$trigger} class="menu-header">
+      <h1 class="title">Projectrix Trialgrounds</h1>
+      <button
+        aria-label={$open ? 'close menu' : 'open menu'}
+        class="material-symbols-outlined menu-button"
+      >
+        {#if $open}
+          unfold_less
+        {:else}
+          unfold_more
+        {/if}
+      </button>
+    </div>
+    {#if $open}
+      <div use:melt={$content} class="menus-container">
+        <nav aria-labelledby="navTitle">
+          <p id="navTitle">directory</p>
+          <ul>
+            <li>
+              <a href="/{$page.url.search}">all trials</a>
+            </li>
+            <li>
+              <a href="/demos{$page.url.search}">all demos</a>
+            </li>
+          </ul>
+        </nav>
+        <div role="menu" aria-labelledby="menuTitle">
+          <p id="menuTitle">options</p>
+          <div class="option-groups">
+            <fieldset aria-label="common options">
+              {#each sharedOptionNames as name}
+                <OptionCheckbox {name}></OptionCheckbox>
+              {/each}
+            </fieldset>
+            <fieldset aria-label="trial options">
+              {#each trialOptionNames as name}
+                <OptionCheckbox {name} irrelevant={!viewingTrials}></OptionCheckbox>
+              {/each}
+            </fieldset>
+          </div>
         </div>
       </div>
-    </div>
+    {/if}
   </div>
 {/if}
 
@@ -81,7 +101,7 @@
   .centerer {
     position: relative;
     width: 100%;
-    z-index: 1;
+    z-index: 3;
 
     display: flex;
     flex-direction: column;
@@ -94,13 +114,50 @@
     }
   }
 
+  .menu-header {
+    all: unset;
+
+    display: flex;
+    margin-top: 1em;
+    border-radius: 0.3em;
+    padding: 0.5em;
+    gap: 0.5em;
+
+    background: hsla(225, 32%, 10%, 0.8);
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+  }
+
   .title {
-    margin-bottom: 0;
+    margin: 0;
+    line-height: 1.3em;
 
     font-family: 'rubik';
     text-align: center;
+  }
 
-    background: hsla(225, 32%, 10%, 0.8);
+  .menu-button {
+    font-size: 1.8em;
+    width: 0.8em;
+    height: 1.1em;
+    line-height: 1.1em;
+    border: none;
+    border-radius: 0.15em;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-weight: 500;
+    color: #111521;
+    background: coral;
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+  }
+  .menu-header:hover {
+    .menu-button {
+      background-color: rgb(223, 109, 68);
+    }
   }
 
   .menus-container {
@@ -108,7 +165,9 @@
     width: fit-content;
     z-index: 1;
 
+    border-radius: 0.3em;
     padding-inline: 0.3em;
+    padding-bottom: 0.4em;
 
     display: flex;
     flex-direction: row;
