5aeb7cb49a4764a02c80b2ebfaeee3267158758f
anxpara <anxpara@gmail.com>
2025-05-12 17:28:17 -0400

    [TG] migrate to SvelteKit 2

--- trialgrounds/svelte.config.js
+++ trialgrounds/svelte.config.js
@@ -1,5 +1,5 @@
 import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
